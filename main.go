41af46ecddc6eef9d9763d8556f9dca9395bb792
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-23 21:56:47 +0100

    Introduction of channels

--- main.go
+++ main.go
@@ -14,16 +14,26 @@ func main() {
 		"https://amazon.com",
 	}
 
+	// create a channel to manage child go routines
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Yes it's up"
 }
