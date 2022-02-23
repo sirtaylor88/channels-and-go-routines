f0ee51cd9f0628539edfe620ba6e6017fc3a9595
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-23 22:27:35 +0100

    Add delay to repeat routines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -27,7 +28,10 @@ func main() {
 	// }
 
 	for l := range c {
-		go checkLink(l, c)
+		go func(lk string) {
+			time.Sleep(5 * time.Second)
+			checkLink(lk, c)
+		}(l)
 	}
 
 }
